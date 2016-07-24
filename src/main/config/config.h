944886a444eb6fabb75d26e75d98e8670e2729f8
Bengt <bengt.mansson@gmail.com>
2016-07-24 14:24:54 +0200

    testing

--- src/main/config/config.h
+++ src/main/config/config.h
@@ -61,5 +61,3 @@ uint8_t getCurrentControlRateProfile(void);
 void changeControlRateProfile(uint8_t profileIndex);
 
 bool canSoftwareSerialBeUsed(void);
-uint16_t getCurrentMinthrottle(void);
-uint16_t getCurrentMaxthrottle(void);
